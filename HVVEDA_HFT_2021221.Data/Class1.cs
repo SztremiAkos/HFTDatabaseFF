92d7227e2dfb8f1e676f9670479e306a656a71a5
Sztrémi Ákos <sz.akos0101@gmail.com>
2021-10-07 17:35:42 +0200

    Add project files.

---
+++ HVVEDA_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HVVEDA_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ HVVEDA_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace HVVEDA_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
