7cddbf6559a93aad6bc4489505825272ca5a48b2
Sztrémi Ákos <sz.akos0101@gmail.com>
2021-10-07 18:12:04 +0200

    mdf and ldf files were created and modified

--- HVVEDA_HFT_2021221.Models/Class1.cs
+++ HVVEDA_HFT_2021221.Models/Class1.cs
@@ -4,5 +4,6 @@ namespace HVVEDA_HFT_2021221.Models
 {
     public class Class1
     {
+        //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Database.mdf;Integrated Security=True
     }
 }
