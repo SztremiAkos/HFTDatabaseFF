1f22d918655432880588a2269ebeb6ba0b0578af
Sztrémi Ákos <sz.akos0101@gmail.com>
2021-11-17 12:43:07 +0100

    studentlogic crud methods done

--- HVVEDA_HFT_2021221.Logic/StudentLogic.cs
+++ HVVEDA_HFT_2021221.Logic/StudentLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HVVEDA_HFT_2021221.Models;
+using HVVEDA_HFT_2021221.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,61 @@ namespace HVVEDA_HFT_2021221.Logic
 {
     public interface IStudentLogic
     {
+        ICollection<Course> GetAllCourses(int id);
+        void AddNewStudent(Student student);
+        Student GetStudentbyId(int id);
+        void DelStudentbyId(int id);
 
+        IList<Student> GetAllStudents();
+
+        public void UpdateStudent(Student student);
     }
-    public interface StudentLogic : IStudentLogic
+    public class StudentLogic : IStudentLogic
     {
+        IStudentRepository studrepo;
+
+        public StudentLogic(IStudentRepository studrepo)
+        {
+            this.studrepo = studrepo;
+        }
+
+        public void AddNewStudent(Student student)
+        {
+            studrepo.AddNewStudent(student);
+        }
+
+        public void DelStudentbyId(int id)
+        {
+            if (id < studrepo.ReadAll().Count())
+                studrepo.DeleteOne(id);
+            else
+                throw new IndexOutOfRangeException("~~~~Index is too big!~~~~");
+        }
+
+        public ICollection<Course> GetAllCourses(int id)
+        {
+            if (id < studrepo.ReadAll().Count())
+                return studrepo.GetAllCourses(id);
+            else
+                throw new IndexOutOfRangeException("~~~~Index is too big!~~~~");
+        }
+
+        public IList<Student> GetAllStudents()
+        {
+            return studrepo.ReadAll().ToList();
+        }
+
+        public Student GetStudentbyId(int id)
+        {
+            if (id < studrepo.ReadAll().Count())
+                return studrepo.GetOne(id);
+            else
+                throw new IndexOutOfRangeException("~~~~Index is too big!~~~~");
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            studrepo.UpdateStudent(student);
+        }
     }
 }
